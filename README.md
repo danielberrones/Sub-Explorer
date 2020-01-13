108835d6c2280b09331eef4d071b869aed7fc24a
Daniel Berrones <Daniel.A.Berrones@gmail.com>
2020-01-13 00:46:41 -0600

    Updated File

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 A tool to search Reddit.  The user enters a SubReddit and the results appear in new window.
 
 
-![](https://media.giphy.com/media/XbCqu8uSVSdngqhJVC/giphy.gif)
+<!-- ![](https://media.giphy.com/media/XbCqu8uSVSdngqhJVC/giphy.gif) -->
+![](https://i.imgur.com/Wyt0osr.gif)
--- src/Sub Explorer.py
+++ src/Sub Explorer.py
@@ -12,8 +12,9 @@ import tkinter as tk
 from tkinter import ttk
 import praw
 
+reddit = praw.Reddit(client_id='xZHnziUldeZEbg', client_secret="P52m77JjNefq4-XVyLEsrkaPGS8",user_agent='carldangerAPI33')
 
-reddit = praw.Reddit(client_id="##YOUR_CLIENT_ID_HERE##", client_secret="##YOUR_CLIENT_SECRET_HERE##",user_agent="YOUR_USER_AGENT_HERE")
+# reddit = praw.Reddit(client_id="##YOUR_CLIENT_ID_HERE##", client_secret="##YOUR_CLIENT_SECRET_HERE##",user_agent="YOUR_USER_AGENT_HERE")
 
 class myApp:
     def __init__(self, master):
